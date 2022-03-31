13676aa7aa69f8de4d58169929a1916873e7e77f
Forrest Sun <internetboy0823@gmail.com>
2022-03-30 17:50:11 -0700

    support follow local origin

--- src/components/ParticleSystemProperties.tsx
+++ src/components/ParticleSystemProperties.tsx
@@ -1,110 +1,90 @@
 import * as React from "react";
-import {ApplicationContextConsumer} from "./ApplicationContext";
 import {GeneratorEditor, GenericGenerator, ValueType} from "./editors/GeneratorEditor";
-import {ParticleSystem, RenderMode} from "three.quarks";
+import {ParticleSystem, TrailSettings, RenderMode} from "three.quarks";
 import {FunctionValueGenerator, ValueGenerator} from "three.quarks";
 import {ColorGenerator, FunctionColorGenerator} from "three.quarks";
+import {Typography} from "@mui/material";
+import {SelectInput} from "./editors/SelectInput";
 
 interface ParticleSystemPropertiesProps {
     particleSystem: ParticleSystem,
-    updateProperties: ()=>void,
+    updateProperties: () => void,
 }
 
-export class ParticleSystemProperties extends React.PureComponent<ParticleSystemPropertiesProps> {
-    constructor(props: Readonly<ParticleSystemPropertiesProps>) {
-        super(props);
-    }
-    onChangeStartSpeed = (g: GenericGenerator) => {
-        this.props.particleSystem.startSpeed = g as ValueGenerator | FunctionValueGenerator;
-        this.props.updateProperties();
+export const ParticleSystemProperties: React.FC<ParticleSystemPropertiesProps> = (props) => {
+    const onChangeStartSpeed = (g: GenericGenerator) => {
+        props.particleSystem.startSpeed = g as ValueGenerator | FunctionValueGenerator;
+        props.updateProperties();
     };
-    onChangeStartLife = (g: GenericGenerator) => {
-        this.props.particleSystem.startLife = g as ValueGenerator | FunctionValueGenerator;
-        this.props.updateProperties();
+    const onChangeStartLife = (g: GenericGenerator) => {
+        props.particleSystem.startLife = g as ValueGenerator | FunctionValueGenerator;
+        props.updateProperties();
     };
-    onChangeStartSize = (g: GenericGenerator) => {
-        this.props.particleSystem.startSize = g as ValueGenerator | FunctionValueGenerator;
-        this.props.updateProperties();
+    const onChangeStartSize = (g: GenericGenerator) => {
+        props.particleSystem.startSize = g as ValueGenerator | FunctionValueGenerator;
+        props.updateProperties();
     };
-    onChangeStartColor = (g: GenericGenerator) => {
-        this.props.particleSystem.startColor = g as ColorGenerator | FunctionColorGenerator;
-        this.props.updateProperties();
+    const onChangeStartColor = (g: GenericGenerator) => {
+        props.particleSystem.startColor = g as ColorGenerator | FunctionColorGenerator;
+        props.updateProperties();
     };
-    onChangeStartRotation = (g: GenericGenerator) => {
-        this.props.particleSystem.startRotation = g as ValueGenerator | FunctionValueGenerator;
-        this.props.updateProperties();
+    const onChangeStartRotation = (g: GenericGenerator) => {
+        props.particleSystem.startRotation = g as ValueGenerator | FunctionValueGenerator;
+        props.updateProperties();
     };
-    onChangeEmissionOverTime = (g: GenericGenerator) => {
-        this.props.particleSystem.emissionOverTime = g as ValueGenerator | FunctionValueGenerator;
-        this.props.updateProperties();
+    const onChangeEmissionOverTime = (g: GenericGenerator) => {
+        props.particleSystem.emissionOverTime = g as ValueGenerator | FunctionValueGenerator;
+        props.updateProperties();
     };
-    onChangeStartLength = (g: GenericGenerator) => {
-        this.props.particleSystem.startLength = g as ValueGenerator | FunctionValueGenerator;
-        this.props.updateProperties();
+    const onChangeStartLength = (g: GenericGenerator) => {
+        (props.particleSystem.rendererEmitterSettings as TrailSettings).startLength = g as ValueGenerator | FunctionValueGenerator;
+        props.updateProperties();
     };
-
-    render() {
-        const valueFunctionTypes: ValueType[] = ['value', 'valueFunc'];
-        const colorValueFunctionTypes: ValueType[] = ['color', 'colorFunc'];
-        return (
-            <div className="property-container">
-                <ApplicationContextConsumer>
-                    {context => context &&
-                        <GeneratorEditor name="Start Life"
-                                         allowedType={valueFunctionTypes}
-                                         value={this.props.particleSystem.startLife}
-                                         onChange={this.onChangeStartLife}/>
-                    }
-                </ApplicationContextConsumer>
-                <ApplicationContextConsumer>
-                    {context => context &&
-                        <GeneratorEditor name="Start Size"
-                                         allowedType={valueFunctionTypes}
-                                         value={this.props.particleSystem.startSize}
-                                         onChange={this.onChangeStartSize}/>
-                    }
-                </ApplicationContextConsumer>
-                {this.props.particleSystem.renderMode === RenderMode.Trail && <ApplicationContextConsumer>
-                    {context => context &&
-                        <GeneratorEditor name="Start Length"
-                                         allowedType={valueFunctionTypes}
-                                         value={this.props.particleSystem.startLength}
-                                         onChange={this.onChangeStartLength}/>
-                    }
-                </ApplicationContextConsumer>}
-                <ApplicationContextConsumer>
-                    {context => context &&
-                        <GeneratorEditor name="Start Speed"
-                                         allowedType={valueFunctionTypes}
-                                         value={this.props.particleSystem.startSpeed}
-                                         onChange={this.onChangeStartSpeed}/>
-                    }
-                </ApplicationContextConsumer>
-                <ApplicationContextConsumer>
-                    {context => context &&
-                        <GeneratorEditor name="Start Color"
-                                         allowedType={colorValueFunctionTypes}
-                                         value={this.props.particleSystem.startColor}
-                                         onChange={this.onChangeStartColor}/>
-                    }
-                </ApplicationContextConsumer>
-                <ApplicationContextConsumer>
-                    {context => context &&
-                        <GeneratorEditor name="Start Rotation"
-                                         allowedType={valueFunctionTypes}
-                                         value={this.props.particleSystem.startRotation}
-                                         onChange={this.onChangeStartRotation}/>
-                    }
-                </ApplicationContextConsumer>
-                <ApplicationContextConsumer>
-                    {context => context &&
-                        <GeneratorEditor name="Emit Over Time"
-                                         allowedType={valueFunctionTypes}
-                                         value={this.props.particleSystem.emissionOverTime}
-                                         onChange={this.onChangeEmissionOverTime}/>
-                    }
-                </ApplicationContextConsumer>
-            </div>
-        );
-    }
-}
+    const onChangeFollowLocalOrigin = (value: string) => {
+        (props.particleSystem.rendererEmitterSettings as TrailSettings).followLocalOrigin = (value === 'True');
+        props.updateProperties();
+    };
+    const valueFunctionTypes: ValueType[] = ['value', 'valueFunc'];
+    const colorValueFunctionTypes: ValueType[] = ['color', 'colorFunc'];
+    return (
+        <div className="property-container">
+            <GeneratorEditor name="Start Life"
+                             allowedType={valueFunctionTypes}
+                             value={props.particleSystem.startLife}
+                             onChange={onChangeStartLife}/>
+            <GeneratorEditor name="Start Size"
+                             allowedType={valueFunctionTypes}
+                             value={props.particleSystem.startSize}
+                             onChange={onChangeStartSize}/>
+            <GeneratorEditor name="Start Speed"
+                             allowedType={valueFunctionTypes}
+                             value={props.particleSystem.startSpeed}
+                             onChange={onChangeStartSpeed}/>
+            <GeneratorEditor name="Start Color"
+                             allowedType={colorValueFunctionTypes}
+                             value={props.particleSystem.startColor}
+                             onChange={onChangeStartColor}/>
+            <GeneratorEditor name="Start Rotation"
+                             allowedType={valueFunctionTypes}
+                             value={props.particleSystem.startRotation}
+                             onChange={onChangeStartRotation}/>
+            {props.particleSystem.renderMode === RenderMode.Trail &&
+                <GeneratorEditor name="Start Length"
+                                 allowedType={valueFunctionTypes}
+                                 value={(props.particleSystem.rendererEmitterSettings as TrailSettings).startLength}
+                                 onChange={onChangeStartLength}/>
+            }
+            {props.particleSystem.renderMode === RenderMode.Trail &&
+                <div className="property">
+                    <Typography component={"label"} className="name">Follow Local Origin</Typography>
+                    <SelectInput onChange={onChangeFollowLocalOrigin}
+                                 value={(props.particleSystem.rendererEmitterSettings as TrailSettings).followLocalOrigin ? "True" : "False"}
+                                 options={["True", "False"]} />
+                </div>}
+            <GeneratorEditor name="Emit Over Time"
+                             allowedType={valueFunctionTypes}
+                             value={props.particleSystem.emissionOverTime}
+                             onChange={onChangeEmissionOverTime}/>
+        </div>
+    );
+};
